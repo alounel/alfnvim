19925eab77b8da4358c5201b31c1c12b8d6b070b
alounel <uhavous@gmail.com>
2023-08-30 18:53:22 +0800

    feat:add compiler.nvim support

--- lua/debugger/lang/dap-bash.lua
+++ lua/debugger/lang/dap-bash.lua
@@ -1,9 +1,11 @@
 local dap = require("dap")
+
 dap.adapters.bashdb = {
     type = "executable",
     command = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/bash-debug-adapter",
     name = "bashdb",
 }
+
 dap.configurations.sh = {
     {
         type = "bashdb",
--- lua/debugger/lang/dap-neovim-lua.lua
+++ lua/debugger/lang/dap-neovim-lua.lua
@@ -1,11 +1,16 @@
 local dap = require("dap")
+
+dap.adapters.nlua = function(callback, config)
+    callback({ type = "server", host = config.host or "127.0.0.0", port = config.port or 8086 })
+end
+
 dap.configurations.lua = {
     {
         type = "nlua",
         request = "attach",
         name = "Attach to running Neovim instance",
+        program = function()
+            pcall(require("osv").launch({ port = 8086 }))
+        end,
     },
 }
-dap.adapters.nlua = function(callback, config)
-    callback({ type = "server", host = config.host, port = config.port })
-end
