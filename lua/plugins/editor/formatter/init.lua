76a2ab775cc790dc678fd21e731b0acafd8dadf8
alounel <uhavous@gmail.com>
2023-08-30 18:23:49 +0800

    remove formatter.nvim

--- lua/plugins/editor/formatter/init.lua
+++ lua/plugins/editor/formatter/init.lua
@@ -1,39 +1,16 @@
 return {
     {
-        "mhartington/formatter.nvim",
-        lazy = true,
-        keys = {
-            { "<leader>fme", "<cmd>Format<CR>", desc = "Format" },
-            { "<leader>fmw", "<cmd>FormatWrite<CR>", desc = "Format Write" },
-            { "<leader>fml", "<cmd>FormatLock<CR>", desc = "Format Lock" },
-            { "<leader>fmn", "<cmd>FormatWriteLock<CR>", desc = "Format Write And Lock" },
-        },
-        config = function()
-            require("formatter").setup({
-                filetype = {
-                    javascript = {
-                        require("formatter.filetypes.javascript").prettier,
-                    },
-                    typescript = {
-                        require("formatter.filetypes.typescript").prettier,
-                    },
-                    markdown = {
-                        require("formatter.filetypes.markdown").prettier,
-                    },
-                },
-            })
-        end,
-    },
-    -- 作为对formatter.nvim的补充
-    {
         "stevearc/conform.nvim",
         lazy = true,
-        ft = { "lua", "python", "sh", "cmake", "json", "yaml" },
+        ft = { "lua", "python", "sh", "cmake", "json", "yaml", "javascript", "typescript", "markdown" },
         config = function()
             require("conform").setup({
                 formatters_by_ft = {
                     cmake = { "cmake_format" },
                     json = { "jq" },
+                    javascript = { "prettier" },
+                    typescript = { "prettier" },
+                    markdown = { "prettier" },
                     lua = { "stylua" },
                     python = {
                         formatters = { "black", "isort" },
@@ -48,6 +25,9 @@ return {
                     },
                 },
             })
+            vim.keymap.set("", "<leader>fm", function()
+                require("conform").format({ async = true, lsp_fallback = true })
+            end, { desc = "Format Buffer" })
             vim.api.nvim_create_autocmd("BufWritePre", {
                 pattern = "*",
                 callback = function(args)
