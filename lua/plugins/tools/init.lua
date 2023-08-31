95510ebf71fe72ea0bd839e1b1dd12eb8a77841c
alounel <uhavous@gmail.com>
2023-08-31 19:59:55 +0800

    feat:add flatten.nvim.adjust codewindow keys

--- lua/plugins/tools/init.lua
+++ lua/plugins/tools/init.lua
@@ -4,21 +4,34 @@ return {
         "gorbit99/codewindow.nvim",
         lazy = true,
         keys = {
-            { "<leader>sm", mode = { "n" }, desc = "Toggle Minimap" },
-            { "<leader>sf", mode = { "n" }, desc = "Focus Minimap" },
+            {
+                "<leader>sm",
+                function()
+                    require("codewindow").toggle_minimap()
+                end,
+                desc = "Toggle Minimap",
+            },
+            {
+                "<leader>sf",
+                function()
+                    require("codewindow").toggle_focus()
+                end,
+                desc = "Focus Minimap",
+            },
         },
         config = function()
             require("codewindow").setup({
                 exclude_filetypes = { "help", "vimcdoc", "log", "txt" },
                 window_border = "rounded",
             })
-            vim.keymap.set("n", "<leader>sm", function()
-                require("codewindow").toggle_minimap()
-            end)
-            vim.keymap.set("n", "<leader>sf", function()
-                require("codewindow").toggle_focus()
-            end)
         end,
+    },
+    -- 从终端缓冲区打开文件
+    {
+        "willothy/flatten.nvim",
+        lazy = true,
+        event = "VeryLazy",
+        opts = {},
     },
     --侧边栏
     {
