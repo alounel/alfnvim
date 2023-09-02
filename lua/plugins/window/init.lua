7819af75a0ffee00c6416df7232972e188e85cbc
alounel <uhavous@gmail.com>
2023-09-02 12:44:00 +0800

    add focus.nvim

---
+++ lua/plugins/window/init.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "nvim-focus/focus.nvim",
+        version = false,
+        lazy = true,
+        keys = {
+            { "<leader>wfg", "<cmd>FocusToggle<CR>", desc = "Toggle Focus" },
+            { "<leader>wfn", "<cmd>FocusSplitNicely<CR>", desc = "Nicely Split Focus" },
+            { "<leader>wfc", "<cmd>FocusSplitCycle<CR>", desc = "Cycle Split Foucus" },
+            { "<leader>wfw", "<cmd>FocusToggleWindow<CR>", desc = "Again Toggle Window" },
+            { "<leader>wfl", ":FocusSplitLeft", desc = "Left Split + Open File Or Custom Command" },
+            { "<leader>wfd", ":FocusSplitDown", desc = "Down Split + Open File Or Custom Command" },
+            { "<leader>wfu", ":FocusSplitUp", desc = "Up Split + Open File Or Custom Command" },
+            { "<leader>wfr", ":FocusSplitRight", desc = "Right Split + Open File Or Custom Command" },
+            { "<leader>wfe", "<cmd>FocusEqualise<CR>", desc = "Temporarily Equalises The Splits" },
+            { "<leader>wfm", "<cmd>FocusMaximise<CR>", desc = "Temporarily Maximises The Focussed Window" },
+            { "<leader>wfo", "<cmd>FocusMaxOrEqual<CR>", desc = "Swtich Equalises And Maximises" },
+        },
+        opts = {},
+    },
+}
