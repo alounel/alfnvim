e5390f25e8eb883bfd7d57e8c7d80869c303fcb4
alounel <uhavous@gmail.com>
2023-09-15 01:02:46 +0800

    refact conform.nvim function

--- lua/plugins/editor/formatter/init.lua
+++ lua/plugins/editor/formatter/init.lua
@@ -2,39 +2,75 @@ return {
     {
         "stevearc/conform.nvim",
         lazy = true,
-        ft = { "lua", "python", "sh", "cmake", "json", "yaml", "javascript", "typescript", "markdown" },
-        config = function()
-            require("conform").setup({
-                formatters_by_ft = {
-                    cmake = { "cmake_format" },
-                    json = { "jq" },
-                    jsonc = { "jq" },
-                    javascript = { "prettier", "prettierd" },
-                    javascriptreact = { "prettier", "prettierd" },
-                    typescript = { "prettier", "prettierd" },
-                    typescriptreact = { "prettier", "prettierd" },
-                    markdown = { "prettier", "prettierd" },
-                    lua = { "stylua" },
-                    python = { "black", "isort" },
-                    sh = { "shellharden", "shfmt" },
-                    yaml = { "yamlfmt" },
-                    ["*"] = { "trim_whitespace" },
-                },
-                log_level = vim.log.levels.DEBUG,
-            })
-            vim.keymap.set("", "<leader>fm", function()
-                require("conform").format({ async = true, lsp_fallback = true })
-            end, { desc = "Format Buffer" })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                pattern = "*",
-                callback = function(args)
-                    local ignore_filetypes = { "c", "cpp", "java", "vim", "xml" }
-                    if vim.tbl_contains(ignore_filetypes, vim.bo[args.buf].filetype) then
-                        return
-                    end
-                    require("conform").format({ timeout_ms = 500, lsp_fallback = true, buf = args.buf })
+        event = "BufWritePre",
+        keys = {
+            {
+                "<leader>fm",
+                function()
+                    require("conform").format({ async = true, lsp_fallback = true })
                 end,
-            })
+                mode = "",
+                desc = "Format buffer",
+            },
+        },
+        opts = {
+            formatters_by_ft = {
+                c = { "clang_format" },
+                cpp = { "clang_format" },
+                cmake = { "cmake_fomat" },
+                json = { "jq" },
+                jsonc = { "jq" },
+                lua = { "stylua" },
+                markdown = { "prettierd", "prettier" },
+                javascript = { "prettierd", "prettier" },
+                javascriptreact = { "prettierd", "prettier" },
+                typescript = { "prettierd", "prettier" },
+                typescriptreact = { "prettierd", "prettier" },
+                python = { "isort", "black" },
+                sh = { "shfmt" },
+                yaml = { "yamlfmt" },
+                ["*"] = { "trim_whitespace" },
+            },
+            log_level = vim.log.levels.DEBUG,
+            format_on_save = function(bufnr)
+                local async_format = vim.g.async_format_filetypes[vim.bo[bufnr].filetype]
+                if async_format or vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return
+                end
+                return { timeout_ms = 500, lsp_fallback = true }
+            end,
+            format_after_save = function(bufnr)
+                local async_format = vim.g.async_format_filetypes[vim.bo[bufnr].filetype]
+                if not async_format or vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return
+                end
+                return { lsp_fallback = true }
+            end,
+            user_async_format_filetypes = {
+                c = true,
+                cpp = true,
+                cmake = true,
+                json = true,
+                jsonc = true,
+                lua = true,
+                markdown = true,
+                javascript = true,
+                javascriptreact = true,
+                typescript = true,
+                typescriptreact = true,
+                python = true,
+                sh = true,
+                yaml = true,
+            },
+        },
+        config = function(_, opts)
+            vim.list_extend(require("conform.formatters.shfmt").args, { "-i", "2" })
+            if vim.g.started_by_firenvim then
+                opts.format_on_save = false
+                opts.format_after_save = false
+            end
+            vim.g.async_format_filetypes = opts.user_async_format_filetypes
+            require("conform").setup(opts)
         end,
     },
 }
