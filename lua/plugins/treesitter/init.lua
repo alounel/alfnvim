8cdcc359df7e8ca256bc0319272acfa971fc8a3a
alounel <uhavous@gmail.com>
2023-09-16 02:42:19 +0800

    optimize

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -138,7 +138,6 @@ return {
         "m-demare/hlargs.nvim",
         lazy = true,
         ft = { "c", "cpp", "java", "javascript", "lua", "python", "typescript", "vim" },
-        dependencies = { "nvim-treesitter" },
         opts = {},
         keys = {
             { "<leader>kr", "<cmd>lua require('hlargs').toggle()<CR>", desc = "Toggle Highlight Args" },
