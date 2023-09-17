24f6deac2b93779e832770cc4bf54796fa445b00
alounel <uhavous@gmail.com>
2023-09-17 20:18:25 +0800

    optimize format settings

--- lua/plugins/editor/formatter/init.lua
+++ lua/plugins/editor/formatter/init.lua
@@ -1,3 +1,13 @@
+local function get_lsp_fallback(bufnr)
+    local formatters = require("conform").list_formatters(bufnr)
+    if #formatters > 0 and formatters[1].name == "trim_whitespace" then
+        return "always"
+    else
+        return true
+    end
+end
+
+local slow_format_filetypes = {}
 return {
     {
         "stevearc/conform.nvim",
@@ -7,62 +17,60 @@ return {
             {
                 "<leader>fm",
                 function()
-                    require("conform").format({ async = true, lsp_fallback = true })
+                    require("conform").format({ async = true, lsp_fallback = get_lsp_fallback(0) })
                 end,
                 mode = "",
-                desc = "Format buffer",
+                desc = "Format Buffer",
             },
         },
-        config = function()
-            local slow_format_filetypes = {}
-            require("conform").setup({
-                formatters_by_ft = {
-                    c = { "clang_format" },
-                    cpp = { "clang_format" },
-                    cmake = { "cmake_format" },
-                    json = { "jq" },
-                    jsonc = { "jq" },
-                    lua = { "stylua" },
-                    markdown = { "markdownlint" },
-                    javascript = { "prettierd", "prettier" },
-                    javascriptreact = { "prettierd", "prettier" },
-                    typescript = { "prettierd", "prettier" },
-                    typescriptreact = { "prettierd", "prettier" },
-                    python = { "isort", "black" },
-                    sh = { "shellharden", "shfmt" },
-                    yaml = { "yamlfmt" },
-                    ["*"] = { "trim_whitespace" },
-                },
-                log_level = vim.log.levels.DEBUG,
-                format_on_save = function(bufnr)
-                    local ignore_filetypes = { "java", "vim", "xml" }
-                    if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
-                        return
+        opts = {
+            formatters_by_ft = {
+                c = { "clang_format" },
+                cpp = { "clang_format" },
+                cmake = { "cmake_format" },
+                json = { "jq" },
+                jsonc = { "jq" },
+                lua = { "stylua" },
+                markdown = { "markdownlint" },
+                javascript = { "prettierd", "prettier" },
+                javascriptreact = { "prettierd", "prettier" },
+                typescript = { "prettierd", "prettier" },
+                typescriptreact = { "prettierd", "prettier" },
+                python = { "isort", "black" },
+                sh = { "shellharden", "shfmt" },
+                yaml = { "yamlfmt" },
+                ["_"] = { "trim_whitespace", "trim_newlines" },
+            },
+            log_level = vim.log.levels.DEBUG,
+            format_on_save = function(bufnr)
+                local ignore_filetypes = { "java", "vim", "xml" }
+                if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+                    return
+                end
+                if slow_format_filetypes[vim.bo[bufnr].filetype] then
+                    return
+                end
+                local function on_format(err)
+                    if err and err:match("timed out$") then
+                        slow_format_filetypes[vim.bo[bufnr].filetype] = true
                     end
-                    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                        return
-                    end
-                    if slow_format_filetypes[vim.bo[bufnr].filetype] then
-                        return
-                    end
-                    local function on_format(err)
-                        if err and err:match("timed out$") then
-                            slow_format_filetypes[vim.bo[bufnr].filetype] = true
-                        end
-                    end
-                    return { timeout_ms = 200, lsp_fallback = true }, on_format
-                end,
-                format_after_save = function(bufnr)
-                    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                        return
-                    end
-                    if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-                        return
-                    end
-                    return { lsp_fallback = true }
-                end,
-            })
+                end
+                return { timeout_ms = 200, lsp_fallback = get_lsp_fallback(bufnr) }, on_format
+            end,
+            format_after_save = function(bufnr)
+                if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+                    return
+                end
+                return { lsp_fallback = get_lsp_fallback(bufnr) }
+            end,
+        },
+        config = function(_, opts)
             vim.list_extend(require("conform.formatters.shfmt").args, { "-i", "4" })
+            if vim.g.started_by_firenvim then
+                opts.format_on_save = false
+                opts.format_after_save = false
+            end
+            require("conform").setup(opts)
         end,
     },
 }
