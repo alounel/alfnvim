4094fece985d44e3aedeea97b4f12670d289e5ab
alounel <uhavous@gmail.com>
2023-09-30 23:55:01 +0800

    add nvim-lint and adjuct none-ls config

--- lua/plugins/editor/linter/init.lua
+++ lua/plugins/editor/linter/init.lua
@@ -1,3 +1,4 @@
+local uv = vim.uv or vim.loop
 return {
     -- 使用lua注入诊断、格式化、补全服务
     {
@@ -35,6 +36,8 @@ return {
             local sources = {
                 -- Code_actions
                 code_actions.eslint_d, -- javascript,javascriptact,typescript,typescriptact,vue
+                code_actions.proselint, -- markdown,tex
+                code_actions.shellcheck, -- sh
                 -- Diagnostics
                 diagnostics.cmake_lint, --cmake
                 diagnostics.eslint, --javascript,javascriptact,typescript,typescriptact,vue
@@ -51,33 +54,15 @@ return {
                 diagnostics.jsonlint, --json
                 diagnostics.yamllint, --yaml
                 diagnostics.vint, --vim
-                diagnostics.cspell.with({
+                diagnostics.codespell.with({
                     disabled_filetypes = {
                         "c",
                         "cpp",
-                        "cmake",
-                        "javascript",
-                        "typescript",
-                        "vue",
-                        "lua",
-                        "markdown",
-                        "python",
-                        "go",
-                        "toml",
-                        "bash",
-                        "sh",
-                        "zsh",
-                        "fish",
-                        "html",
-                        "xml",
-                        "css",
-                        "json",
-                        "yaml",
-                        "yml",
-                        "vim",
-                        "java",
-                        "rust",
                         "norg",
+                        "text",
+                        "toml",
+                        "vim",
+                        "yaml",
                     },
                 }),
                 diagnostics.editorconfig_checker.with({
@@ -97,6 +82,48 @@ return {
                 },
                 diagnostics_format = "[#{c}] #{m} (#{s})",
             })
+        end,
+    },
+    -- 补充c,cpp的代码诊断
+    {
+        "mfussenegger/nvim-lint",
+        lazy = true,
+        ft = { "c", "cpp" },
+        opts = {
+            linters_by_ft = {
+                c = { "cppcheck" },
+                cpp = { "cppcheck" },
+            },
+            linters = {},
+        },
+        config = function(_, opts)
+            local lint = require("lint")
+            lint.linters_by_ft = opts.linters_by_ft
+            for k, v in pairs(opts.linters) do
+                lint.linters[k] = v
+            end
+            local timer = assert(uv.new_timer())
+            local DEBOUNCE_MS = 500
+            local aug = vim.api.nvim_create_augroup("lint", { clear = true })
+            vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertEnter" }, {
+                group = aug,
+                callback = function()
+                    local bufnr = vim.api.nvim_get_current_buf()
+                    timer:stop()
+                    timer:start(
+                        DEBOUNCE_MS,
+                        0,
+                        vim.schedule_wrap(function()
+                            if vim.api.nvim_buf_is_valid(bufnr) then
+                                vim.api.nvim_buf_call(bufnr, function()
+                                    lint.try_lint(nil, { ignore_errors = true })
+                                end)
+                            end
+                        end)
+                    )
+                end,
+            })
+            lint.try_lint(nil, { ignore_errors = true })
         end,
     },
     --显示代码诊断, 参考, telescope结果, quickfix和位置列表
