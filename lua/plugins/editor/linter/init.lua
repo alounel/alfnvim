91e35a85e0c9a8743b04d89379bb380356a115da
alounel <uhavous@gmail.com>
2023-09-30 23:58:23 +0800

    optimize

--- lua/plugins/editor/linter/init.lua
+++ lua/plugins/editor/linter/init.lua
@@ -21,7 +21,7 @@ return {
                             "markdownlint",
                             "shellcheck",
                             "jsonlint",
-                            "cspell",
+                            "proselint",
                             "editorconfig_checker",
                         },
                     })
