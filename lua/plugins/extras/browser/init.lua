47bf3cc60313b08d14cc337e426884cd12f1bb57
alounel <uhavous@gmail.com>
2023-10-09 18:26:18 +0800

    add mini.files

--- lua/plugins/extras/browser/init.lua
+++ lua/plugins/extras/browser/init.lua
@@ -8,4 +8,31 @@ return {
             { "<leader>nz", "<cmd>RnvimrResize<CR>", desc = "Resize Ranger in Window" },
         },
     },
+    -- 文件导航
+    {
+        "echasnovski/mini.files",
+        version = false,
+        lazy = true,
+        keys = {
+            {
+                "<leader>nm",
+                "<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0), true)<CR>",
+                desc = "Open Mini Files(Current File Dir)",
+            },
+            {
+                "<leader>nM",
+                "<cmd>lua MiniFiles.open(vim.loop.cwd(), true)<CR>",
+                desc = "Open Mini Files(Root Dir)",
+            },
+        },
+        config = function()
+            require("mini.files").setup({
+                windows = {
+                    preview = true,
+                    width_focus = 30,
+                    width_preview = 30,
+                },
+            })
+        end,
+    },
 }
