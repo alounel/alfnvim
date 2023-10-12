609b217bdd8be48d64e9154b98fb561b862584e7
alounel <uhavous@gmail.com>
2023-10-12 11:59:00 +0800

    adjust conform.nvim format config

--- lua/plugins/extras/editor/formatter.lua
+++ lua/plugins/extras/editor/formatter.lua
@@ -7,7 +7,17 @@ return {
             {
                 "<leader>fm",
                 function()
-                    require("conform").format({ async = true, lsp_fallback = true })
+                    require("conform").format({ async = true, lsp_fallback = true }, function(err)
+                        if not err then
+                            if vim.startswith(vim.api.nvim_get_mode().mode:lower(), "v") then
+                                vim.api.nvim_feedkeys(
+                                    vim.api.nvim_replace_termcodes("<Esc>", true, false, true),
+                                    "n",
+                                    true
+                                )
+                            end
+                        end
+                    end)
                 end,
                 mode = "",
                 desc = "Format Buffer",
