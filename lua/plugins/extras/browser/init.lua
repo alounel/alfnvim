e2c2e31d20b04bf23a838e0fb3c83178d276fad2
alounel <uhavous@gmail.com>
2023-10-15 21:32:26 +0800

    remove rnvimr

--- lua/plugins/extras/browser/init.lua
+++ lua/plugins/extras/browser/init.lua
@@ -1,14 +1,5 @@
 return {
-    -- 在悬浮窗中运行ranger
-    {
-        "kevinhwang91/rnvimr",
-        lazy = true,
-        keys = {
-            { "<leader>nr", "<cmd>RnvimrToggle<CR>", desc = "Toggle Ranger in Float Window" },
-            { "<leader>nz", "<cmd>RnvimrResize<CR>", desc = "Resize Ranger in Window" },
-        },
-    },
-    -- 文件导航
+    -- 文件浏览和导航
     {
         "echasnovski/mini.files",
         version = false,
