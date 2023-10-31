492d137214c2fd4c87e7f6c651d314fffca3d80c
alounel <uhavous@gmail.com>
2023-10-31 12:07:15 +0800

    optimize config

--- lua/plugins/extras/develop/bigfile.lua
+++ lua/plugins/extras/develop/bigfile.lua
@@ -3,19 +3,21 @@ return {
     {
         "LunarVim/bigfile.nvim",
         priority = 100,
-        opts = {
-            filesize = 2,
-            pattern = { "*" },
-            features = {
-                "indent_blankline",
-                "illuminate",
-                "lsp",
-                "treesitter",
-                "syntax",
-                "matchparen",
-                "vimopts",
-                "filetype",
-            },
-        },
+        config = function()
+            require("bigfile").setup({
+                filesize = 2,
+                pattern = { "*" },
+                features = {
+                    "indent_blankline",
+                    "illuminate",
+                    "lsp",
+                    "treesitter",
+                    "syntax",
+                    "matchparen",
+                    "vimopts",
+                    "filetype",
+                },
+            })
+        end,
     },
 }
