55193583b5c4b7e21bf0cffbcdb75b404db50222
alounel <uhavous@gmail.com>
2023-11-02 00:53:54 +0800

    optimize nvim-bqf config

--- lua/plugins/elevate/init.lua
+++ lua/plugins/elevate/init.lua
@@ -20,13 +20,60 @@ return {
         "kevinhwang91/nvim-bqf",
         lazy = true,
         ft = "qf",
+        init = function()
+            function _G.qftf(info)
+                local items
+                local ret = {}
+                if info.quickfix == 1 then
+                    items = vim.fn.getqflist({ id = info.id, items = 0 }).items
+                else
+                    items = vim.fn.getloclist(info.winid, { id = info.id, items = 0 }).items
+                end
+                local limit = 31
+                local fnameFmt1, fnameFmt2 = "%-" .. limit .. "s", "…%." .. (limit - 1) .. "s"
+                local validFmt = "%s │%5d:%-3d│%s %s"
+                for i = info.start_idx, info.end_idx do
+                    local e = items[i]
+                    local fname = ""
+                    local str
+                    if e.valid == 1 then
+                        if e.bufnr > 0 then
+                            fname = vim.fn.bufname(e.bufnr)
+                            if fname == "" then
+                                fname = "[No Name]"
+                            else
+                                fname = fname:gsub("^" .. vim.env.HOME, "~")
+                            end
+                            if #fname <= limit then
+                                fname = fnameFmt1:format(fname)
+                            else
+                                fname = fnameFmt2:format(fname:sub(1 - limit))
+                            end
+                        end
+                        local lnum = e.lnum > 99999 and -1 or e.lnum
+                        local col = e.col > 999 and -1 or e.col
+                        local qtype = e.type == "" and "" or " " .. e.type:sub(1, 1):upper()
+                        str = validFmt:format(fname, lnum, col, qtype, e.text)
+                    else
+                        str = e.text
+                    end
+                    table.insert(ret, str)
+                end
+                return ret
+            end
+
+            vim.o.qftf = "{info -> v:lua._G.qftf(info)}"
+        end,
         config = function()
             vim.cmd([[
                 hi BqfPreviewBorder guifg=#3e8e2d ctermfg=71
                 hi BqfPreviewTitle guifg=#3e8e2d ctermfg=71
                 hi BqfPreviewThumb guibg=#3e8e2d ctermbg=71
+                hi BqfPreviewSbar guifg=#ae4d55 ctermfg=172
+                hi BqfPreviewCursor guifg=#d75f00 ctermfg=166
                 hi link BqfPreviewRange Search
             ]])
+
             require("bqf").setup({
                 auto_enable = true,
                 auto_resize_height = true,
@@ -35,6 +82,7 @@ return {
                     win_vheight = 12,
                     delay_syntax = 80,
                     border = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
+                    show_title = false,
                 },
                 func_map = {
                     vsplit = "",
@@ -43,8 +91,8 @@ return {
                 },
                 filter = {
                     fzf = {
-                        action_for = { ["ctrl-s"] = "split" },
-                        extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+                        action_for = { ["ctrl-s"] = "split", ["ctrl-t"] = "tab drop" },
+                        extra_opts = { "--bind", "ctrl-o:toggle-all", "--delimiter", "│" },
                     },
                 },
             })
