c61b33ad6ca00acb38f0ff06a59be3ab97966bb1
alounel <uhavous@gmail.com>
2023-11-19 11:56:27 +0800

    Optimization

--- lua/plugins/extras/treesitter/commentstring.lua
+++ lua/plugins/extras/treesitter/commentstring.lua
@@ -2,8 +2,10 @@ return {
     {
         "JoosepAlviste/nvim-ts-context-commentstring",
         lazy = true,
+        init = function()
+            vim.g.skip_ts_context_commentstring_module = true
+        end,
         opts = {
-            enable = true,
             enable_autocmd = false,
         },
     },
